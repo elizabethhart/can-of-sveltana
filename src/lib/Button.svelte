1eb4c2a9b3dcb6a3c8a3ba8f56742cba1bc56a0f
elizabethhart <elizabethwhart@gmail.com>
2022-02-22 22:08:58 -0600

    Add component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let style: string = 'primary';
+	export let disabled: boolean = false;
+	export let type: string = 'button';
+</script>
+
+<button {type} {disabled} class={style}><slot /></button>
+
+<style>
+	button {
+		color: #fff;
+		border: 0;
+		border-radius: 8px;
+		color: #fff;
+		width: 200px;
+		height: 60px;
+		cursor: pointer;
+	}
+
+	.primary {
+		background-color: #2d4085;
+	}
+
+	.secondary {
+		background: #bcb4cc;
+	}
+
+	button:hover {
+		transform: scale(0.98);
+		opacity: 0.9;
+	}
+
+	button:disabled {
+		background-color: #cccccc;
+		color: #333;
+		cursor: auto;
+	}
+
+	button:disabled:hover {
+		transform: scale(1);
+		opacity: 1;
+	}
+</style>
---
+++ src/routes/library.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	import Button from '$lib/Button.svelte';
+</script>
+
+<svelte:head>
+	<title>Library</title>
+</svelte:head>
+
+<div class="content">
+	<section>
+		<h2>Buttons</h2>
+		<div class="content-item"><Button style="primary">Primary Button</Button></div>
+		<div class="content-item"><Button style="secondary">Secondary Button</Button></div>
+		<div class="content-item"><Button style="primary" disabled={true}>Disabled Button</Button></div>
+	</section>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.content-item {
+		margin: 5px;
+	}
+</style>
